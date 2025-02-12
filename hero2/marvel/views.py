19bf2b25883151b69ea73b302e958bb30269f0f3
Pramesh Gurav <prameshgurav79286@gmail.com>
2025-02-12 12:45:32 +0530

    1. project of Django

---
+++ hero2/marvel/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def ironman(request):
+    return HttpResponse("<h1>I am ironman</h1>")
+
+def spiderman(request):
+    return HttpResponse("<h1>I am spiderman</h1>")
+
+
