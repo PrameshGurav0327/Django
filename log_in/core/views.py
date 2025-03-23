630f06747d56b94a485c73ec0f3956b9233aee00
Pramesh Gurav <prameshgurav79286@gmail.com>
2025-03-23 17:36:42 +0530

    -

--- log_in/core/views.py
+++ log_in/core/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
 from .forms import Register
 from django.contrib import messages
+from django.contrib.auth import authenticate, login,logout
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -15,3 +17,35 @@ def signup(request):
     else:
         mf = Register()
     return render(request,"core/index.html",{"mf":mf})
+
+def log_in(request):
+    if not request.method == "POST":
+        mf = AuthenticationForm(request,data=request.POST)
+        if mf.is_valid():
+            un = mf.cleaned_data["username"]
+            pw = mf.cleaned_data["password"]
+            user=authenticate(username=un,password=pw)
+            if user is not None:
+                login(request,user)
+                return redirect("/profile/")
+            else:
+                mf=AuthenticationForm()
+            return render(request,"core/log_in.html",{"mf":mf})
+        else:
+            return redirect("/profile/")
+        
+def profile(request):
+    if request.user.is_authenticated:
+        return render(request,"core/profile.html")
+    else:
+        return redirect("/login/")
+    
+def log_out(request):
+    logout(request)
+    return redirect("/login/")
+
+def success(request):
+    return render(request,"core/success.html")
+
+    
+
